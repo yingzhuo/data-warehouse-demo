ee0a62e76ad37d1e3068aa33a0b3f087f1ecc3bf
yingzhuo <yingzhor@gmail.com>
2021-01-13 10:59:43 +0800

    2021-01-13 10:59:41

--- 02-data-warehouse/ddl/03-hive-creation-dws.sql
+++ 02-data-warehouse/ddl/03-hive-creation-dws.sql
@@ -51,3 +51,30 @@ create external table dws_user_action_daycount
     stored as parquet
     location '/hive/data-warehouse-demo/dws/dws_user_action_daycount'
     tblproperties ('parquet.compression' = 'lzo');
+
+---
+-- 每日商品行为汇总
+---
+drop table if exists dws_commodity_action_daycount;
+create external table dws_commodity_action_daycount
+(
+    `commodity_id`       string comment '商品ID',
+    `order_count`        bigint comment '被下单次数',
+    `order_num`          bigint comment '被下单件数',
+    `order_amount`       decimal(16, 2) comment '被下单金额',
+    `payment_count`      bigint comment '被支付次数',
+    `payment_num`        bigint comment '被支付件数',
+    `payment_amount`     decimal(16, 2) comment '被支付金额',
+    `cart_count`         bigint comment '被加购次数',
+    `favorite_count`     bigint comment '收藏次数',
+    `evaluation_count_1` bigint comment '被好评次数',
+    `evaluation_count_2` bigint comment '被中评次数',
+    `evaluation_count_3` bigint comment '被差评次数'
+)
+    comment '每日商品行为汇总'
+    partitioned by (`dt` string comment '日期分区')
+    row format delimited
+        fields terminated by '\001'
+    stored as parquet
+    location '/hive/data-warehouse-demo/dws/dws_commodity_action_daycount'
+    tblproperties ('parquet.compression' = 'lzo');
---
+++ 02-data-warehouse/scripts/bak/demo.sql
@@ -0,0 +1,58 @@
+select commodity_id,
+       count(*)                 order_count, -- 被下单次数
+       sum(count)               order_num,   -- 被下单件数
+       sum(count * final_price) order_amount
+from dwd_fact_order_item_db
+where dt = '2020-12-31'
+group by commodity_id;
+
+select commodity_id,
+       count(*)                 order_count, -- 被下单次数
+       sum(count)               order_num,   -- 被下单件数
+       sum(count * final_price) order_amount
+from dwd_fact_order_item_db
+where dt = '2020-12-31'
+  and order_id in
+      (
+          select order_id
+          from dwd_fact_payment_info_db
+          where dt = '2020-12-31'
+      )
+group by commodity_id;
+
+select commodity_id,
+       count(*) cart_count -- 商品被加购次数
+from dwd_fact_cart_item_db
+where dt = '2020-12-31'
+group by commodity_id;
+
+select commodity_id,
+       count(*) fav_count -- 商品被收藏次数
+from dwd_fact_favor_info_db
+where dt = '2020-12-31'
+group by commodity_id;
+
+
+select
+    oi.commodity_id as commodity_id,
+    sum(if(e.level = '好评', 1, 0)) as evaluation_count_1,
+    sum(if(e.level = '中评', 1, 0)) as evaluation_count_2,
+    sum(if(e.level = '差评', 1, 0)) as evaluation_count_3
+from dwd_fact_order_item_db oi
+         left join dwd_fact_evaluation_db e
+                   on oi.order_id = e.order_id
+where
+    oi.dt = '2020-12-31' and e.dt = '2020-12-31'
+group by
+    oi.commodity_id;
+
+select
+    oi.commodity_id as commodity_id,
+    sum(if(e.level = '好评', 1, 0)) as evaluation_count_1,
+    sum(if(e.level = '中评', 1, 0)) as evaluation_count_2,
+    sum(if(e.level = '差评', 1, 0)) as evaluation_count_3
+from dwd_fact_order_item_db oi
+         left join dwd_fact_evaluation_db e
+                   on oi.order_id = e.order_id
+group by
+    oi.commodity_id
